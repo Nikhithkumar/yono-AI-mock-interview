b21111934bea19dda2120e4cb4d42501f4eaccda
Nikhith kumar <120315182+Nikhithkumar@users.noreply.github.com>
2025-02-23 22:47:09 +0530

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -2,7 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const sql = neon(process.env.DATABASE_URL ?? 'postgresql://neondb_owner:npg_GBpkmdDW0Ft8@ep-wandering-night-a5hx5z8m-pooler.us-east-2.aws.neon.tech/Yono_AI_Mock_Interview?sslmode=require');
+const sql = neon(process.env.DATABASE_URL;
 
 // Corrected drizzle call
 export const db = drizzle(sql, { schema });
